4117a4088115bb9c2d96f56758633100887847ed
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-02-27 18:06:54 +0300

    tabWidth 4

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,13 +3,27 @@ import { motion } from 'framer-motion';
 import styles from './Link.module.css';
 
 interface Props {
-  name: string;
+    name: string;
 }
 
+const slashMotion = {
+    rest: { opacity: 0, ease: 'easeOut', duration: 0.2, type: 'tween' },
+    hover: {
+        opacity: 1,
+        transition: {
+            duration: 0.4,
+            type: 'tween',
+            ease: 'easeIn',
+        },
+    },
+};
+
 export const Link = ({ name }: Props): JSX.Element => {
-  return (
-    <ParallaxElement as="li" className={styles.link}>
-      <motion.a href="">{name}</motion.a>
-    </ParallaxElement>
-  );
+    return (
+        <ParallaxElement as="li" className={styles.link}>
+            <motion.a variants={slashMotion} initial="rest" whileHover="hover" animate="rest" href="">
+                {name}
+            </motion.a>
+        </ParallaxElement>
+    );
 };
