198b9c9070a3d02b30901aa0dce86801fde400c1
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-03-22 15:29:42 +0300

    обновил package.json

--- src/pages/Contacts/Contacts.tsx
+++ src/pages/Contacts/Contacts.tsx
@@ -10,7 +10,7 @@ export const Contacts = (): JSX.Element => {
                 <div className={styles.titleContainer}>
                     <div className={styles.imageContainer}>
                         <img src="./kelly-sikkema-7alo7OJVNVw-unsplash.jpg" alt="logo" />
-                        <Title tag="h2"> Lorem ipsum</Title>
+                        <Title tag="h1"> Lorem ipsum</Title>
                     </div>
                     <Title tag="h2">dolor sit amet consectetur</Title>
                     <ArrowIcon className={styles.arrow} />
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,9 +1,21 @@
+import { motion, useScroll, useTransform } from 'framer-motion';
 import { ContactSection, AboutSection } from '../../sections';
+import styles from './Main.module.css';
 
 export const Main = (): JSX.Element => {
+    const { scrollY } = useScroll();
+
+    const heightSizes = [96, 0];
+    const offsetY = [0, 100];
+
+    const height = useTransform(scrollY, offsetY, heightSizes);
+
     return (
         <>
             <AboutSection />
+            <motion.div className={styles.footerMain} style={{ height: height }}>
+                {/* <div className={styles.rounded}></div> */}
+            </motion.div>
             <ContactSection />
         </>
     );
