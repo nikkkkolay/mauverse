8cabfce5935b1ace8b6c1ef1161e794c8c17c4ae
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-03-29 14:15:11 +0300

    вёрстка сайдбара

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,16 @@
-import { Social } from '..';
+import { MenuLink, Social } from '..';
 import styles from './Sidebar.module.css';
 
 export const Sidebar = (): JSX.Element => {
     return (
         <div className={styles.sidebar}>
             <div className={styles.sidebarInner}>
+                <p>Меню</p>
+                <ul className={styles.routes}>
+                    <MenuLink path="/" name="Lorem" className={styles.link} type={'menu'} />
+                    <MenuLink path="/" name="Lorem" className={styles.link} />
+                    <MenuLink path="/" name="Lorem" className={styles.link} />
+                </ul>
                 <Social />
             </div>
         </div>
