ad9867b365ea784ad30e05c1e4d7edc5bd20996a
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-04-01 18:27:33 +0300

    базовая анимация sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,13 +2,17 @@ import { MenuLink, Social } from '..';
 import { useSidebar } from '../../store/useSidebar';
 import classnames from 'classnames';
 import styles from './Sidebar.module.css';
-import { motion } from 'framer-motion';
+import { cubicBezier, motion } from 'framer-motion';
 
 export const Sidebar = (): JSX.Element => {
     const isOpen = useSidebar(state => state.isActive);
 
     return (
-        <motion.div className={classnames(styles.sidebar, { [styles.active]: isOpen })}>
+        <motion.div
+            className={classnames(styles.sidebar, { [styles.active]: isOpen })}
+            animate={{ x: isOpen ? 0 : 'calc(100% + 6vw)' }}
+            transition={{ type: 'liner', duration: 0.5, delayChildren: 0.5 }}
+        >
             <div className={styles.sidebarInner}>
                 <p className={styles.nav}>Меню</p>
                 <ul className={styles.routes}>
