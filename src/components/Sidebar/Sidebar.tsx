331199e174b103c2efcf87465596ea9427d725b9
Гагара Николай Федорович <ggn.f764@gmail.com>
2024-04-01 18:43:58 +0300

    плавность анимации

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,24 +2,27 @@ import { MenuLink, Social } from '..';
 import { useSidebar } from '../../store/useSidebar';
 import classnames from 'classnames';
 import styles from './Sidebar.module.css';
-import { cubicBezier, motion } from 'framer-motion';
+import { motion } from 'framer-motion';
+import { useRef } from 'react';
 
 export const Sidebar = (): JSX.Element => {
     const isOpen = useSidebar(state => state.isActive);
+    const sidebar = useRef(null);
 
     return (
         <motion.div
             className={classnames(styles.sidebar, { [styles.active]: isOpen })}
             animate={{ x: isOpen ? 0 : 'calc(100% + 6vw)' }}
-            transition={{ type: 'liner', duration: 0.5, delayChildren: 0.5 }}
+            transition={{ ease: [0.5, 0, 0.2, 1], duration: 0.7, delayChildren: 0.5 }}
+            ref={sidebar}
         >
             <div className={styles.sidebarInner}>
                 <p className={styles.nav}>Меню</p>
-                <ul className={styles.routes}>
+                <motion.ul className={styles.routes}>
                     <MenuLink path="/" name="Lorem" className={styles.link} />
                     <MenuLink path="/" name="Lorem" className={styles.link} />
                     <MenuLink path="/" name="Lorem" className={styles.link} />
-                </ul>
+                </motion.ul>
                 <Social />
             </div>
         </motion.div>
