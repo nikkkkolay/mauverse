6213ba4f2eccf45fee6303e257a5f8d189fc0a0e
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-05-01 22:16:16 +0300

    вёрстка страницы контактов

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const Social = ({ className }: Props): JSX.Element => {
     return (
-        <Info title={'Lorem ipsum'} className={className}>
+        <Info title={'Social'} className={className}>
             <ul className={styles.links}>
                 <ListItem name="Lorem ipsum" path="#" className={styles.link} />
                 <ListItem name="Lorem" path="#" className={styles.link} />
--- src/pages/ContactsPage/ContactsPage.tsx
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Title, Form } from '../../components';
+import { Container, Title, Form, Social, Info, ListItem } from '../../components';
 import { ArrowIcon } from '../../icons/ArrowIcon';
 
 import styles from './ContactsPage.module.css';
@@ -15,7 +15,22 @@ export const ContactsPage = (): JSX.Element => {
                     <Title tag="h2">dolor sit amet consectetur</Title>
                     <ArrowIcon className={styles.arrow} />
                 </div>
-                <Form />
+                <div className={styles.row}>
+                    <div className={styles.col}>
+                        <Form />
+                    </div>
+                    <div className={styles.col}>
+                        <Info title={'Contacts'}>
+                            <ul className={styles.links}>
+                                <ListItem name="mail@mail.net" path="#" className={styles.link} />
+                                <ListItem name="Dolor sit amet consectetur" path="#" className={styles.link} />
+                                <ListItem name="Lorem ipsum amet" path="#" className={styles.link} />
+                                <ListItem name="8 999 999 99" path="#" className={styles.link} />
+                            </ul>
+                        </Info>
+                        <Social className={styles.social} />
+                    </div>
+                </div>
             </Container>
         </section>
     );
