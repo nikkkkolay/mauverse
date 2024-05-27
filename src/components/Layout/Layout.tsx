9d8092c858378adc7017ff50a4fa260b9629fd67
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-05-27 17:26:07 +0300

    перенёс бургер меню в компонент лейаута что бы исправить баг с фиксед  положением

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,32 +1,43 @@
+import { useRef } from 'react';
+import { useInView } from 'framer-motion';
 import { Outlet, useLocation } from 'react-router-dom';
 import classnames from 'classnames';
-import { Footer, Navbar, Overlay, PageTransition, Sidebar, ScrollToTop, SmoothScroll } from '../';
+import { Footer, Navbar, Overlay, PageTransition, Sidebar, ScrollToTop, SmoothScroll, Hamburger } from '../';
 import styles from './Layout.module.css';
+import { useSidebar } from '../../store/useSidebar';
 
 export const Layout = (): JSX.Element => {
+    const ref = useRef(null);
+
     const location = useLocation();
+    const isInView = useInView(ref);
+
+    const isActive = useSidebar(state => state.isActive);
 
     return (
         <PageTransition>
-            <SmoothScroll>
-                <ScrollToTop />
+            <>
+                <SmoothScroll>
+                    <ScrollToTop />
 
-                <header
-                    className={classnames(styles.header, {
-                        [styles.headerHeight]: location.pathname === '/',
-                        [styles.fillBlack]: location.pathname === '/contacts',
-                        [styles.fillWhite]: location.pathname === '/about',
-                    })}
-                >
-                    <Navbar />
-                    {location.pathname === '/' && <Overlay />}
-                </header>
-                <main className={styles.main}>
-                    <Outlet />
-                </main>
-                <Footer />
+                    <header
+                        className={classnames(styles.header, {
+                            [styles.headerHeight]: location.pathname === '/',
+                            [styles.fillBlack]: location.pathname === '/contacts',
+                            [styles.fillWhite]: location.pathname === '/about',
+                        })}
+                    >
+                        <Navbar ref={ref} />
+                        {location.pathname === '/' && <Overlay />}
+                    </header>
+                    <main className={styles.main}>
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </SmoothScroll>
                 <Sidebar />
-            </SmoothScroll>
+                <Hamburger isInView={!isInView || isActive} />
+            </>
         </PageTransition>
     );
 };
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,26 @@
-import { useRef } from 'react';
+import { forwardRef } from 'react';
 import classnames from 'classnames';
-import { Hamburger, MenuLink } from '..';
-import { useInView } from 'framer-motion';
+import { MenuLink } from '..';
 import { useSidebar } from '../../store/useSidebar';
 import { routs } from '../../routs';
 import styles from './Navbar.module.css';
 
-export const Navbar = (): JSX.Element => {
+export const Navbar = forwardRef((props, ref: any): JSX.Element => {
     const isActive = useSidebar(state => state.isActive);
     const setActive = useSidebar(state => state.setActive);
-
-    const ref = useRef(null);
-    const isInView = useInView(ref);
 
     return (
         <nav className={styles.nav} ref={ref}>
             <img className={styles.logo} src="" alt="Lorem" />
             <ul className={styles.routes}>
-                {routs && routs.map((rout: { path: string; name: string; id: number }) => <MenuLink key={rout.id} path={rout.path} name={rout.name} />)}
+                {routs &&
+                    routs.map((rout: { path: string; name: string; id: number }) => <MenuLink key={rout.id} path={rout.path} name={rout.name} />)}
             </ul>
             <ul className={classnames(styles.routes, styles.visible)}>
                 <li className={classnames({ [styles.colorWhite]: isActive })} onClick={() => setActive(!isActive)}>
                     Menu
                 </li>
             </ul>
-            <Hamburger isInView={!isInView} />
         </nav>
     );
-};
+});
