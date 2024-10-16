dbd0e4ee3e1e07a1192f12d2796c87d40765207e
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-10-16 15:56:35 +0300

    загрузил картинки и добавил мета информацию

--- src/components/MetaHelmet/MetaHelmet.tsx
+++ src/components/MetaHelmet/MetaHelmet.tsx
@@ -2,26 +2,16 @@ import { Helmet } from 'react-helmet';
 
 interface IMetaHelmet {
     title: string;
-    description: string;
+    description?: string;
     keywords?: string;
-    canonical?: string;
-    type?: string;
-    image?: string;
 }
 
-export const MetaHelmet = ({ title, keywords, description, canonical, type, image }: IMetaHelmet): JSX.Element => {
+export const MetaHelmet = ({ title, keywords, description }: IMetaHelmet): JSX.Element => {
     return (
         <Helmet>
-            <meta charSet="utf-8" />
             <title>{title}</title>
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
-            <meta property="og:description" content={description} />
-            <meta property="og:site_name" content="Lorem" />
-            {canonical && <link rel="canonical" href={canonical} />}
-            {canonical && <meta property="og:url" content={canonical} />}
-            {type && <meta property="og:type" content={type} />}
-            {image && <meta property="og:image" content={image} />}
         </Helmet>
     );
 };
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -4,7 +4,7 @@ import { AboutSection, NewsSection } from '../../sections';
 export const MainPage = (): JSX.Element => {
     return (
         <>
-            <MetaHelmet title={'Lorem'} description={'Lorem ipsum'} />
+            <MetaHelmet title={'MAUverse'} />
             <AboutSection />
             <NewsSection />
         </>
