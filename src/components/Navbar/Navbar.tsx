a339b6c2977a73e78d4b545b98d0e27bb1d42c21
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-10-18 18:16:05 +0300

    добавил лого белого цвета

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,30 @@
 import { forwardRef } from 'react';
 import classnames from 'classnames';
+import { useLocation } from 'react-router-dom';
 import { MenuLink } from '..';
 import { useSidebar } from '../../store/useSidebar';
 import { publicRoutes } from '../../routs';
 import styles from './Navbar.module.css';
 
 export const Navbar = forwardRef((_props, ref: any): JSX.Element => {
+    const location = useLocation();
     const isActive = useSidebar(state => state.isActive);
     const setActive = useSidebar(state => state.setActive);
 
     return (
         <nav className={styles.nav} ref={ref}>
             <a href="https://mauniver.ru" target="_blank">
-                <img className={styles.logoFull} src="/logo_rus_full_black_H.png" alt="Лого МАУ" />
-                <img className={styles.logoAbb} src="/logo_rus_abb_black_H.png" alt="Лого МАУ" />
+                {location.pathname === '/contacts' || location.pathname === '/404' ? (
+                    <>
+                        <img className={styles.logoFull} src="/logo_rus_full_white_H.png" alt="Лого МАУ" />
+                        <img className={styles.logoAbb} src="/logo_rus_abb_white_H.png" alt="Лого МАУ" />
+                    </>
+                ) : (
+                    <>
+                        <img className={styles.logoFull} src="/logo_rus_full_black_H.png" alt="Лого МАУ" />
+                        <img className={styles.logoAbb} src="/logo_rus_abb_black_H.png" alt="Лого МАУ" />
+                    </>
+                )}
             </a>
             <ul className={styles.routes}>
                 {publicRoutes &&
