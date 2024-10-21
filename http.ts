f7b44988ba7c750f4a2c79e9b127a662cd15a5ef
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-10-21 10:09:04 +0300

    изменил baseUrl

--- http.ts
+++ http.ts
@@ -2,6 +2,4 @@ import axios from 'axios';
 
 export const API_URL = import.meta.env.VITE_JSON_SERVER_URL;
 
-export const api = axios.create({
-    baseURL: API_URL,
-});
+export const api = axios.create({ baseURL: API_URL });
