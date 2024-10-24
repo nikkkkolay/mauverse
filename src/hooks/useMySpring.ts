aeb09a652b7fd055f56a2ce70d7dd8458a5fc81c
Гагара Николай Федорович <gagaranf@mauniver.ru>
2024-10-24 15:48:48 +0300

    убрал спринг на моб

--- src/hooks/useMySpring.ts
+++ src/hooks/useMySpring.ts
@@ -1,5 +1,18 @@
 import { useSpring, MotionValue } from 'framer-motion';
+import { useLayoutEffect, useState } from 'react';
 
-export const useMySpring = (value: MotionValue<number>, stiffness: number = 80, damping: number = 25) => {
-    return useSpring(value, { stiffness: stiffness, damping: damping });
+export const useMySpring = (value: MotionValue<number>, stiffness: number = 60, damping: number = 50) => {
+    const [isMobile, setIsMobile] = useState(typeof window !== 'undefined' && window.innerWidth <= 780);
+    const springValue = useSpring(value, { stiffness, damping });
+
+    useLayoutEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 780);
+        };
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return isMobile ? value : springValue;
 };
