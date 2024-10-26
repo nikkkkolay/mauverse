6908bfcc71e89126749fd6c3acd6bbf784de10bc
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-10-26 22:45:15 +0300

    vite конфиг для оптимизации чанков

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    'mui-vendor': ['@mui/material', '@mui/icons-material'],
+                    'react-vendor': ['react', 'react-dom', 'react-router-dom', 'react-helmet', 'zustand'],
+                    'motion-vendor': ['framer-motion', '@motionone/utils'],
+                    'data-handling': [
+                        'axios',
+                        'ra-data-json-server',
+                        'ra-data-simple-rest',
+                        'ra-i18n-polyglot',
+                        'ra-input-rich-text',
+                        'ra-language-russian',
+                        'react-hook-form',
+                    ],
+                    'admin-panel': ['react-admin'],
+                },
+            },
+        },
+    },
 });
