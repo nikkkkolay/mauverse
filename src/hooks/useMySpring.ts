48bc38ba1bba87481419f78f316fd534ac397326
turboreaktivnayatyaga <ggn.f764@gmail.com>
2024-10-27 15:28:07 +0300

    размер текста в элементе загрузки и плавность анимации

--- src/hooks/useMySpring.ts
+++ src/hooks/useMySpring.ts
@@ -1,20 +1,5 @@
 import { useSpring, MotionValue } from 'framer-motion';
-import { useEffect, useState } from 'react';
 
-export const useMySpring = (value: MotionValue<number>, stiffness: number = 100, damping: number = 30, mass: number = 1) => {
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 780);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 780);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-    }, []);
-
-    const mobileSettings = isMobile ? { stiffness: 200, damping: 40, mass: 0.8 } : { stiffness, damping, mass };
-    const springValue = useSpring(value, mobileSettings);
-
-    return springValue;
+export const useMySpring = (value: MotionValue<number>, stiffness: number = 140, damping: number = 30, mass: number = 1) => {
+    return useSpring(value, { stiffness, damping, mass });
 };
